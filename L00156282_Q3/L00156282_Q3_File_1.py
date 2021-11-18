8396d8929a2ad0284710199e7303e98217b84eb9
ciaranmcdevittlyit <L00156282@student.lyit.ie>
2021-11-18 09:47:58 +0000

    Assignment Q3 & Q4

---
+++ L00156282_Q3/L00156282_Q3_File_1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import paramiko
+from datetime import datetime
+import credentials as creds
+
+ip_address = "192.168.178.130"
+
+
+def ssh_connection(ip, datetime_string):
+    """
+    ssh connection function accepts an IP address and a datetime_string and connects over SSH to create a
+    connection.txt file containing the text "Connection Made" and also containing the time of the creation/overwriting
+    USERNAME & PASSWORD are imported from an external 'credentials.py' file
+
+    :param ip: (str) - IP address
+    :param datetime_string: (str) - date & time string
+
+    """
+    try:
+        # please add username and password on the credentials.py file to run
+        username = creds.USERNAME
+        password = creds.PASSWORD
+        print("Establishing a connection...")
+        session = paramiko.SSHClient()
+        session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        session.connect(ip.rstrip("\n"), username=username, password=password)
+        session.invoke_shell()
+        session.exec_command(f"echo Connection Made - {datetime_string} > connection.txt\n")
+        session.close()
+    except paramiko.AuthenticationException:
+        print("Authentication Error")
+
+
+def get_datetime_string():
+    """
+    Function to retrieve the current date and time in a neatly formatted layout
+
+    :return: (str) datetime - formatted date & time
+
+    """
+    now = datetime.now()
+    return now.strftime("%d/%m/%Y %H:%M:%S")
+
+
+def main():
+    """
+    main method calls the get_datetime_string function and passes the datetime_string and the IP address to the
+    ssh_connection function
+
+    """
+    datetime_string = get_datetime_string()
+    ssh_connection(ip_address, datetime_string)
+
+
+if __name__ == "__main__":
+    main()
---
+++ L00156282_Q4/L00156282_Q4_File_1.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import socket
+import subprocess
+import sys
+from datetime import datetime
+
+ip_address = "192.168.178.130"
+
+# set port scan range here
+starting_port = 1
+finishing_port = 1037
+
+# ports dictionary, key = port number, value = protocol
+ports_dictionary = {
+    20: "FTP",  # File Transfer Protocol - Data Transfer
+    21: "FTP",  # File Transfer Protocol - Command Control
+    22: "SSH",  # Secure Shell
+    23: "Telnet",  # Remote login service, unencrypted text messages
+    25: "SMTP",  # Simple Mail Transfer Protocol - E-mail Routing
+    53: "DNS",  # Domain Name System - service
+    80: "HTTP",  # Hypertext Transfer Protocol - used in World Wide Web
+    110: "POP3",  # Post Office Protocol - used by e-mail clients to retrieve e-mail from a server
+    119: "NNTP",  # Network News Transfer Protocol 
+    123: "NTP",  # Network Time Protocol 
+    143: "IMAP",  # Internet Message Access Protocol - Management of Digital Mail
+    161: "SNMP",  # Simple Network Management Protocol 
+    194: "IRC",  # Internet Relay Chat 
+    443: "HTTPS",  # HTTP Secure - HTTP over TLS/SSL
+}
+
+
+def port_scan(remote_server):
+    """
+    run a port scan between a starting and ending range, only if a port is open should the protocol be outputted,
+    run a timer on the port scan and output the run time at the end.  Handle a scenario where a port might be open but
+    has not value in the ports_dictionary
+
+    :param remote_server: ip address of the remote server the port scan will be run on
+    """
+    # Clear the screen
+    subprocess.call("cls", shell=True)
+    remote_server_ip = socket.gethostbyname(remote_server)
+    # display scanning message and IP address
+    print("-" * 60)
+    print("Please wait, scanning remote host", remote_server_ip)
+    print("-" * 60)
+    # Take a reading of the time the scan started
+    scan_start_time = datetime.now()
+    try:
+        print("Open ports")
+        for port in range(starting_port, finishing_port):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((remote_server_ip, port))
+            if result == 0:
+                if port in ports_dictionary:
+                    print(" -{}".format(ports_dictionary[port]))
+                else:
+                    print(f" -Port {port} is open but has no protocol in the ports dictionary")
+            sock.close()
+    except KeyboardInterrupt:
+        print("You pressed Ctrl+C")
+        sys.exit()
+    except socket.gaierror:
+        print('\nHostname could not be resolved. Exiting')
+        sys.exit()
+    except socket.error:
+        print("\nCouldn't connect to server")
+        sys.exit()
+    # Take a reading of the time the scan completed
+    scan_end_time = datetime.now()
+    # Calculates the difference of time, to see how long it took to run the script
+    time_taken = scan_end_time - scan_start_time
+    # Printing the information to screen
+    print('\nScanning Completed in: ', time_taken)
+
+
+if __name__ == "__main__":
+    # if no IP is passed then the user should be asked to enter one
+    if ip_address is None or ip_address == "":
+        ip_address = input("Enter a remote host to scan: ")
+
+    port_scan(ip_address)
